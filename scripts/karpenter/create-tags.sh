af3b7b9ed26b2ca20310d1b072043b9f66e84e62
guessi <guessi@gmail.com>
2023-03-04 22:25:45 +0800

    Make it easy to create tags for Karpenter's subnets

--- scripts/karpenter/create-tags.sh
+++ scripts/karpenter/create-tags.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-CLUSTER_NAME="eks-demo"
+AWS_REGION="us-east-1"
+EKS_CLUSTER_NAME="eks-demo"
 
-TARGET_RESOURCES=(
-  "subnet-XXXXXXXXXXXXXXXXX"
-  "subnet-XXXXXXXXXXXXXXXXX"
-  "sg-XXXXXXXXXXXXXXXXX"
-)
+VPC_ID=$(aws eks describe-cluster --name "${EKS_CLUSTER_NAME}" --region "${AWS_REGION}" --query 'cluster.resourcesVpcConfig.vpcId' --output text --no-cli-pager)
+echo "[debug] ${VPC_ID}"
 
-for TARGET_RESOURCE in ${TARGET_RESOURCES[@]}; do
+PRIVATE_SUBNET_IDS=($(aws ec2 describe-subnets --region "${AWS_REGION}" --filter "Name=state,Values=available" "Name=vpc-id,Values=${VPC_ID}" "Name=tag:kubernetes.io/role/internal-elb,Values=1" --query 'Subnets[*]["SubnetId"][]' --output text --no-cli-pager))
+echo "[debug] ${PRIVATE_SUBNET_IDS[@]}"
+
+for TARGET in ${PRIVATE_SUBNET_IDS[@]}; do
+  echo "[debug] Creating resource tag for \"${TARGET}\""
   aws ec2 create-tags \
-    --tags "Key=karpenter.sh/discovery,Value=${CLUSTER_NAME}" \
-    --resources "${TARGET_RESOURCE}"
+    --tags "Key=karpenter.sh/discovery,Value=${EKS_CLUSTER_NAME}" \
+    --resources "${TARGET}"
 done
+
+echo
+echo "*** YOU WILL STILL NEED TO CREATE TAG FOR SECURITY GROUP FOR NODES ***"
+echo
