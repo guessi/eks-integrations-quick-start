ed94a9b373cb98b03ab8439354cf0fde9268ad7f
guessi <guessi@gmail.com>
2023-07-15 10:18:07 +0800

    Bump default aws-load-balancer-controller version - v2.5.4

--- scripts/aws-load-balancer-controller/setup.sh
+++ scripts/aws-load-balancer-controller/setup.sh
@@ -9,7 +9,8 @@ SERVICE_ACCOUNT_NAME="${SERVICE_ACCOUNT_NAME_AwsLoadBalancerController}"
 
 # CHART VERSION	APP VERSION
 # ---------------------------
-# 1.5.4        	v2.5.3 (recommend)
+# 1.5.5        	v2.5.4 (recommend)
+# 1.5.4        	v2.5.3 
 # 1.5.3        	v2.5.2
 # 1.5.2        	v2.5.1
 # 1.5.1        	v2.5.1
@@ -35,8 +36,8 @@ SERVICE_ACCOUNT_NAME="${SERVICE_ACCOUNT_NAME_AwsLoadBalancerController}"
 #
 # ref: https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.5/deploy/installation/#kubernetes-version-requirements
 
-APP_VERSION="v2.5.3"
-CHART_VERSION="1.5.4"
+APP_VERSION="v2.5.4"
+CHART_VERSION="1.5.5"
 
 echo "[debug] detecting AWS Account ID"
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
