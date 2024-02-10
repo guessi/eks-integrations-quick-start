bf7b5b78931eff394a2d2706c8883545e23a19cb
guessi <guessi@gmail.com>
2024-02-11 00:16:23 +0800

    Simplify setup for aws-node-termination-handler

--- scripts/aws-node-termination-handler/setup.sh
+++ scripts/aws-node-termination-handler/setup.sh
@@ -2,52 +2,30 @@
 
 # CHART VERSION	APP VERSION
 # ---------------------------
-# CHART VERSION	APP VERSION
-# 0.22.0       	1.20.0      # ref: https://github.com/aws/aws-node-termination-handler/releases/tag/v1.20.0
-# 0.21.0       	1.19.0      # ref: https://github.com/aws/aws-node-termination-handler/releases/tag/v1.19.0
+# 0.23.0       	1.21.0
+# 0.22.0       	1.20.0
 
-# APP_VERSION="1.20.0"
+# ref: https://github.com/aws/aws-node-termination-handler/releases/tag/v1.21.0
+# ref: https://github.com/aws/aws-node-termination-handler/tree/v1.21.0/config/helm/aws-node-termination-handler
+
+APP_VERSION="1.20.0" # image for 1.21.0 is not yet published
 CHART_VERSION="0.22.0"
-
-echo "[debug] detecting chart repo existance"
-helm repo list | grep -q 'eks-charts'
-
-if [ $? -ne 0 ]; then
-  echo "[debug] setup chart repo"
-  helm repo add eks https://aws.github.io/eks-charts || true
-else
-  echo "[debug] found chart repo"
-fi
-
-echo "[debug] helm repo update"
-helm repo update eks
-
-echo "[debug] detecting namespace existance"
-kubectl get namespace | grep -q 'aws-node-termination-handler'
-
-if [ $? -ne 0 ]; then
-  echo "[debug] creating namespace"
-  kubectl create namespace aws-node-termination-handler
-else
-  echo "[debug] found namespace"
-fi
 
 echo "[debug] detecting Helm resource existance"
 helm list --all-namespaces | grep -q 'aws-node-termination-handler'
 
-# TODO: nice to have regional image setup
 echo "[debug] setup eks/aws-node-termination-handler"
 helm upgrade \
-  --namespace aws-node-termination-handler \
+  --namespace kube-system \
   --install aws-node-termination-handler \
   --version ${CHART_VERSION} \
-  eks/aws-node-termination-handler \
-    --set enableSpotInterruptionDraining="true" \
-    --set enableRebalanceMonitoring="true" \
-    --set enableRebalanceDraining="true" \
-    --set enableScheduledEventDraining="true" \
-    --set nodeSelector."eks\.amazonaws\.com/capacityType"=SPOT
-   # --set nodeSelector."karpenter\.sh/capacity-type"=spot
+  oci://public.ecr.aws/aws-ec2/helm/aws-node-termination-handler
+    # --set enableSpotInterruptionDraining="true" \
+    # --set enableRebalanceMonitoring="true" \
+    # --set enableRebalanceDraining="true" \
+    # --set enableScheduledEventDraining="true" \
+    # --set nodeSelector."eks\.amazonaws\.com/capacityType"=SPOT
+    # --set nodeSelector."karpenter\.sh/capacity-type"=spot
 
 echo "[debug] listing installed"
 helm list --all-namespaces --filter aws-node-termination-handler
