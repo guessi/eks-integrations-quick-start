c809c1074474dd222b5271d77048e8b74ea0df57
guessi <guessi@gmail.com>
2025-01-03 09:09:43 +0800

    Bump default aws-node-termination-handler version - v1.23.1

--- scripts/aws-node-termination-handler/setup.sh
+++ scripts/aws-node-termination-handler/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-APP_VERSION="1.22.1"
-CHART_VERSION="0.24.1"
+APP_VERSION="1.23.1"
+CHART_VERSION="0.25.1"
 
 echo "[debug] log into Public ECR"
 aws ecr-public get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin public.ecr.aws
