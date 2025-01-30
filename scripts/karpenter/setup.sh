4f157792901eb9856110737717ea674b027885fb
guessi <guessi@gmail.com>
2025-01-30 12:13:10 +0800

    Bump karpenter versions - v1.2.0

--- scripts/karpenter/setup.sh
+++ scripts/karpenter/setup.sh
@@ -19,17 +19,17 @@ KARPENTER_NAMESPACE="kube-system"
 # - https://karpenter.sh/v1.0/upgrading/v1-migration/
 # - https://karpenter.sh/preview/upgrading/compatibility/
 
-
 # CHART VERSION             APP VERSION
 # ----------------------------------------
-# karpenter-v1.1.2          1.1.2    # ref: https://github.com/aws/karpenter/releases/tag/v1.1.2 (recommend)
+# karpenter-v1.2.0          1.2.0    # ref: https://github.com/aws/karpenter/releases/tag/v1.2.0 (recommend)
+# karpenter-v1.1.2          1.1.2    # ref: https://github.com/aws/karpenter/releases/tag/v1.1.2
 # karpenter-v1.0.8          1.0.8    # ref: https://github.com/aws/karpenter/releases/tag/v1.0.8
 # karpenter-v0.37.6         0.37.6   # ref: https://github.com/aws/karpenter/releases/tag/v0.37.6
 # karpenter-v0.36.8         0.36.8   # ref: https://github.com/aws/karpenter/releases/tag/v0.36.8
 # karpenter-v0.35.11        0.35.11  # ref: https://github.com/aws/karpenter/releases/tag/v0.35.11
 
-APP_VERSION="1.1.2"
-CHART_VERSION="1.1.2"
+APP_VERSION="1.2.0"
+CHART_VERSION="1.2.0"
 
 echo "[debug] detecting AWS Account ID"
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
