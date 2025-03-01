8fc8d9f18cfbde6fa591d34ea5450e864d48195c
guessi <guessi@gmail.com>
2025-03-01 13:36:14 +0800

    Bump karpenter versions - v1.2.2

--- scripts/karpenter/setup.sh
+++ scripts/karpenter/setup.sh
@@ -21,15 +21,15 @@ KARPENTER_NAMESPACE="kube-system"
 
 # CHART VERSION             APP VERSION
 # ----------------------------------------
-# karpenter-v1.2.1          1.2.1    # ref: https://github.com/aws/karpenter/releases/tag/v1.2.1 (recommend)
-# karpenter-v1.1.3          1.1.3    # ref: https://github.com/aws/karpenter/releases/tag/v1.1.3
-# karpenter-v1.0.8          1.0.8    # ref: https://github.com/aws/karpenter/releases/tag/v1.0.8
+# karpenter-v1.2.2          1.2.2    # ref: https://github.com/aws/karpenter/releases/tag/v1.2.2 (recommend)
+# karpenter-v1.1.4          1.1.4    # ref: https://github.com/aws/karpenter/releases/tag/v1.1.4
+# karpenter-v1.0.9          1.0.9    # ref: https://github.com/aws/karpenter/releases/tag/v1.0.9
 # karpenter-v0.37.7         0.37.7   # ref: https://github.com/aws/karpenter/releases/tag/v0.37.7
 # karpenter-v0.36.9         0.36.9   # ref: https://github.com/aws/karpenter/releases/tag/v0.36.9
 # karpenter-v0.35.12        0.35.12  # ref: https://github.com/aws/karpenter/releases/tag/v0.35.12
 
-APP_VERSION="1.2.1"
-CHART_VERSION="1.2.1"
+APP_VERSION="1.2.2"
+CHART_VERSION="1.2.2"
 
 echo "[debug] detecting AWS Account ID"
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
