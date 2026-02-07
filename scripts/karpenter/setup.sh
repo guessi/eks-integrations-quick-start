c11251c89721de929f817a350d9aeeb603c8bc3b
guessi <guessi@gmail.com>
2026-02-07 10:23:11 +0800

    Drop legacy karpenter versions

--- scripts/karpenter/setup.sh
+++ scripts/karpenter/setup.sh
@@ -24,8 +24,6 @@ KARPENTER_NAMESPACE="kube-system"
 # karpenter-v1.9.0          1.9.0    # ref: https://github.com/aws/karpenter/releases/tag/v1.9.0 (recommend)
 # karpenter-v1.8.6          1.8.6    # ref: https://github.com/aws/karpenter/releases/tag/v1.8.6
 # karpenter-v1.7.4          1.7.4    # ref: https://github.com/aws/karpenter/releases/tag/v1.7.4
-# karpenter-v1.6.6          1.6.6    # ref: https://github.com/aws/karpenter/releases/tag/v1.6.6
-# karpenter-v1.5.6          1.5.4    # ref: https://github.com/aws/karpenter/releases/tag/v1.5.6
 
 APP_VERSION="1.9.0"
 CHART_VERSION="1.9.0"
