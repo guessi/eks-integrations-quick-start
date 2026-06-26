3256aa94efd4c7853a6f87d3ed31bab89cc07fff
guessi <guessi@gmail.com>
2026-06-26 09:45:31 +0800

    Update supported cluster-autoscaler versions

--- scripts/cluster-autoscaler/setup.sh
+++ scripts/cluster-autoscaler/setup.sh
@@ -9,17 +9,17 @@ SERVICE_ACCOUNT_NAME="${SERVICE_ACCOUNT_NAME_ClusterAutoScaler}"
 
 # CHART VERSION	APP VERSION
 # ---------------------------
-# 9.57.0       	1.35.0
+# 9.58.0       	1.35.1
 
-APP_VERSION="v1.35.0"
-CHART_VERSION="9.57.0"
+APP_VERSION="v1.35.1"
+CHART_VERSION="9.58.0"
 
 # HINT: if there have multiple cluster-autoscaler running under the same cluster, you might need to customize these variables.
 NAMESPACE="kube-system"
 FULLNAME_OVERRIDE="cluster-autoscaler"
 
 # Supported Versions:
-# - https://github.com/kubernetes/autoscaler/releases/tag/cluster-autoscaler-1.35.0
+# - https://github.com/kubernetes/autoscaler/releases/tag/cluster-autoscaler-1.35.1
 # - https://github.com/kubernetes/autoscaler/releases/tag/cluster-autoscaler-1.34.3
 # - https://github.com/kubernetes/autoscaler/releases/tag/cluster-autoscaler-1.33.4
 # - https://github.com/kubernetes/autoscaler/releases/tag/cluster-autoscaler-1.32.7
